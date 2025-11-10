53dbf7a7c787db62b79e081a9bda5eec9ae81793
Laísa Silva <ferreirlaisa@gmail.com>
2025-11-09 22:26:36 -0300

    first commit

---
+++ src/main/java/com/igorfabricia/IgorFabriciaApplication.java
@@ -0,0 +1,13 @@
+package com.igorfabricia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgorFabriciaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IgorFabriciaApplication.class, args);
+    }
+}
+
